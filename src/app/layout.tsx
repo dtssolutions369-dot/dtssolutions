07300c54584b02e4b31c14e744db61a25b017876
Vyshnavi <vyshnavivgowda@gmail.com>
2026-06-02 11:08:51 +0530

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Ecommerce Business Platform",
+  title: "Dts Solutions",
   description: "Business profile based ecommerce website",
+  icons: {
+    icon: "/logo.png", // Points to public/logo.png
+  },
 };
 
 export default function RootLayout({
